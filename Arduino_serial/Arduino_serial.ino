bf897b1838693db17329785477e87ab695416516
Ertekin Şener <ertekinsener22@gmail.com>
2023-10-05 15:12:47 +0300

    Added arduino code
    $$𝑆𝑦𝑠𝑡𝑒𝑚𝑆𝑡𝑎𝑡𝑒, 𝐿𝑒𝑑𝑆𝑡𝑎𝑡𝑒1, 𝐿𝑒𝑑𝑆𝑡𝑎𝑡𝑒2, 𝐵𝑢𝑡𝑡𝑜𝑛𝑃𝑟𝑒𝑠𝑠𝐶𝑜𝑢𝑛𝑡 ∗∗
    Example serial message: $$1,Off,On,5**

--- Arduino_serial/Arduino_serial.ino
+++ Arduino_serial/Arduino_serial.ino
@@ -1,9 +1,46 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
 
+const int buttonPin = 2;
+const int led1Pin = 3;
+const int led2Pin = 4;
+volatile int systemState = 0;
+int buttonPressCount = 0;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonInterrupt, FALLING);
+
+  Serial.begin(9600);  
+  sendStatus();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Your main code can go here if needed.
+}
 
+void buttonInterrupt() {
+  systemState = (systemState + 1) % 4;
+  buttonPressCount++; // Increment button press count
+  updateLEDs();
+  sendStatus();
+}
+
+void updateLEDs() {
+  digitalWrite(led1Pin, (systemState & 0x01) ? HIGH : LOW);
+  digitalWrite(led2Pin, (systemState & 0x02) ? HIGH : LOW);
+}
+
+void sendStatus() {
+  Serial.print("$$");
+  Serial.print(systemState);
+  Serial.print(",");
+  Serial.print((systemState & 0x01) ? "On" : "Off");
+  Serial.print(",");
+  Serial.print((systemState & 0x02) ? "On" : "Off");
+  Serial.print(",");
+  Serial.print(buttonPressCount);  
+  Serial.println("**");
 }
